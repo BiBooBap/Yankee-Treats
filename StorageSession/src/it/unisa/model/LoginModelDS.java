5c95043f17029c7e598c631545c22dd518ad4d21
giudsv <giuseppedsv@gmail.com>
2024-05-17 14:46:28 +0200

    Modifiche alla pagina di registrazione

---
+++ StorageSession/src/it/unisa/model/LoginModelDS.java
@@ -0,0 +1,48 @@
+package it.unisa.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class LoginModelDS {
+
+    private static DataSource ds;
+    private static final String TABLE_NAME = "users";
+
+    static {
+        try {
+            Context initCtx = new InitialContext();
+            Context envCtx = (Context) initCtx.lookup("java:comp/env");
+
+            ds = (DataSource) envCtx.lookup("jdbc/storage");
+
+        } catch (NamingException e) {
+            System.out.println("Error:" + e.getMessage());
+        }
+    }
+
+    public boolean validateUser(LoginBean loginBean) {
+        String query = "SELECT * FROM " + TABLE_NAME + " WHERE email = ? AND password = ?";
+
+        try (Connection conn = ds.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+
+            stmt.setString(1, loginBean.getEmail());
+            stmt.setString(2, loginBean.getPassword());
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                return rs.next();
+            }
+        } catch (SQLException e) {
+            System.out.println("Error validating user: " + e.getMessage());
+            return false;
+        }
+    }
+}
+
