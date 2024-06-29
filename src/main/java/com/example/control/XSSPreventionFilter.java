f6beadcc193975da3dc57fb7fe2094d6a118bb6b
BiBooBap <antomaio19@gmail.com>
2024-06-29 04:35:27 +0200

    Forms and resources validation (anti XSS scripting)

---
+++ src/main/java/com/example/control/XSSPreventionFilter.java
@@ -0,0 +1,79 @@
+package com.example.control;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+public class XSSPreventionFilter implements Filter {
+
+    private static final Pattern[] XSS_PATTERNS = {
+            Pattern.compile("<script>(.*?)</script>", Pattern.CASE_INSENSITIVE),
+            Pattern.compile("src[\r\n]*=[\r\n]*\\\'(.*?)\\\'", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
+            Pattern.compile("src[\r\n]*=[\r\n]*\\\"(.*?)\\\"", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
+            Pattern.compile("</script>", Pattern.CASE_INSENSITIVE),
+            Pattern.compile("<script(.*?)>", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
+            Pattern.compile("eval\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
+            Pattern.compile("expression\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
+            Pattern.compile("javascript:", Pattern.CASE_INSENSITIVE),
+            Pattern.compile("vbscript:", Pattern.CASE_INSENSITIVE),
+            Pattern.compile("onload(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL)
+    };
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        chain.doFilter(new XSSRequestWrapper((HttpServletRequest) request), response);
+    }
+
+    @Override
+    public void destroy() {}
+
+    private static class XSSRequestWrapper extends HttpServletRequestWrapper {
+
+        public XSSRequestWrapper(HttpServletRequest servletRequest) {
+            super(servletRequest);
+        }
+
+        @Override
+        public String[] getParameterValues(String parameter) {
+            String[] values = super.getParameterValues(parameter);
+            if (values == null) {
+                return null;
+            }
+            int count = values.length;
+            String[] encodedValues = new String[count];
+            for (int i = 0; i < count; i++) {
+                encodedValues[i] = stripXSS(values[i]);
+            }
+            return encodedValues;
+        }
+
+        @Override
+        public String getParameter(String parameter) {
+            String value = super.getParameter(parameter);
+            return stripXSS(value);
+        }
+
+        @Override
+        public String getHeader(String name) {
+            String value = super.getHeader(name);
+            return stripXSS(value);
+        }
+
+        private String stripXSS(String value) {
+            if (value != null) {
+                value = value.replaceAll("\0", "");
+                for (Pattern pattern : XSS_PATTERNS) {
+                    value = pattern.matcher(value).replaceAll("");
+                }
+                value = value.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+            }
+            return value;
+        }
+    }
+}
